7e2417324e97ffa518567c29ee89a34ebe336b03
txtszas <txtszas@gmail.com>
2013-03-11 19:10:00 +0800

    rss fixed

--- wp-content/themes/wan/rss.php
+++ wp-content/themes/wan/rss.php
@@ -4,6 +4,6 @@
 	<li><a href="http://www.xianguo.com/subscribe.php?url=http://feed.feedsky.com/efph" target="_blank" title="订阅到鲜果"><img src="<?php echo get_template_directory_uri(); ?>/images/xg-logo.png">鲜果联播</a></li>
 	<li><a href="http://yuedu.163.com/instantCustomSub.do?customUrl=http%3A%2F%2Ffeed.feedsky.com%2Fefph" target="_blank" title="订阅到网易云阅读"><img src="<?php echo get_template_directory_uri(); ?>/images/yiyun-logo.jpg">网易云阅读</a></li>
 	<li><a href="http://fusion.google.com/add?feedurl=http://feed.feedsky.com/efph" target="_blank" title="订阅到谷歌阅读"><img src="<?php echo get_template_directory_uri(); ?>/images/googlereader-logo.png">谷歌阅读</a></li>
-	<li><a href="http://dakahui.com/?feed=rss2" target="_blank" title="全文订阅"><img src="<?php echo get_template_directory_uri(); ?>/images/rss.png">RSS订阅</a></li>
+	<li><a href="http://dakahui.com/feed/atom" target="_blank" title="全文订阅"><img src="<?php echo get_template_directory_uri(); ?>/images/rss.png">RSS订阅</a></li>
 </ul>
 </aside>
